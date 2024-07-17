99524b4d11ca7c4d8307814f9f098369bb802579
devmak122 <devm37878@gmail.com>
2024-07-17 15:31:01 +0530

    update

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -2,8 +2,95 @@ import React from 'react';
 
 const Clients = () => (
   <div className="p-4">
-    <h2 className="text-3xl font-bold">Our Clients</h2>
-    <p>Skyline Enterprises serves a variety of clients...</p>
+    <div class="relative">
+        <img src="assets/images/Group 1000001788.svg" class="w-[1920px] h-[600px]" alt="Description of the image"/>
+        <div class="absolute inset-0 flex items-center justify-center text-white text-[96px] font-bold">
+            CLIENTS
+        </div>
+      </div>
+      <div class="container mx-auto py-8">
+        <h1 class="text-center text-[48px] font-bold mb-8 text-customGreen pb-12" >Our Client's</h1>
+
+        <div class="grid grid-cols-3 gap-6 md:grid-cols-4 lg:grid-cols-4 mx-4 my-4 ml-11">
+
+            <div class="relative group">
+            <div class="absolute flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px] rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/ABB_logo.svg 1.svg" alt="ABB Ltd Group" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">ABB Ltd<br/>Group</p>
+            </div>
+            <div class="bg-green-500 h-[72px] w-[72px] mt-44 ml-[-10px]"></div>
+            </div>
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/download-removebg-preview 1.svg" alt="Tebian Electric Apparatus" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">Tebian Electric<br/>Apparatus</p>
+            </div>
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img3.svg" alt="Prolec Energy" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">Prolec<br/>Energy</p>
+            </div>
+            <div class="relative group">
+            <div class="absolute flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/company4img.svg" alt="Raychem RPG (P) Ltd." class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">Raychem RPG (p)<br/>Ltd.</p>
+            </div>
+            <div class="bg-green-100 h-[72px] w-[72px] mt-[-85px] ml-[170px]"></div>
+        </div>
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img5.svg" alt="ATLANTA ELECTRICALS" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">ATLANTA<br/>ELECTRICALS</p>
+            </div>
+            
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img6.svg" alt="RattanIndia Enterprises" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">RattanIndia<br/>Enterprises</p>
+            </div>
+            <div class="relative">
+            <div class="absolute flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img7.svg" alt="TMC Transformers SpA" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">TMC Transformers<br/>Spa</p>
+            </div>
+            <div class="bg-green-100 h-[80px] w-[80px] mt-[-25px] ml-[299px]"></div>
+        </div>
+            <div class="relative group">
+            <div class=" absolute flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img8.svg" alt="Schneider Electric" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">Schneider<br/>Electric</p>
+            </div>
+            <div class="border-t-[12px] border-r-[12px] border-customGreen h-[72px] w-[72px] mt-[-15px] ml-[256px]"></div>
+            </div>
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img9.svg" alt="CG Power and Industrial Solutions" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">CG Power and<br/>Industrial Solutions</p>
+            </div>
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img10.svg" alt="Shree Cement" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">Shree<br/>Cement</p>
+            </div>
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen  my-2 leading-10">
+                <img src="assets/images/img11.svg" alt="Electrotherm India Ltd." class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">Electrotherm<br/>India Ltd.</p>
+            </div>
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img12.svg" alt="Kirloskar Group" class="h-20 scale-110"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">Kirloskar<br/>Group</p>
+            </div>
+            <div class="relative group">
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img13.svg" alt="Bridgestone Manufacturing" class="h-20"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">Bridgestone<br/>Manufacturing</p>
+            </div>
+            <div class="border-b-[12px] border-l-[12px] border-orange-600 h-[72px] w-[72px] mt-[-55px] ml-[-15px]"></div>
+        </div>
+            <div class=" flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img14.svg" alt="Shilchar Technologies" class="h-20 scale-125		"/>
+                <p class="text-[30px] text-center text-customGreen font-bold pt-3">Shilchar<br/>Technologies</p>
+            </div>
+            <div class="flex flex-col items-center justify-center p-4 bg-white shadow-md w-[296px] h-[215px]  rounded-lg border-[3px] border-customGreen mx-2 my-2 leading-10">
+                <img src="assets/images/img15.svg" alt="Tesla Transformers" class="h-20 scale-125"/>
+                <p class="text-[30px] text-center text-customGreen font-bold  pt-3">Tesla<br/>Transformers</p>
+            </div>
+        </div>
+    </div>
   </div>
 );
 
